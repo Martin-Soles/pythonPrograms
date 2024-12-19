f3d343a12d8966b39f35a908f2e6489122dc690d
martinpicus <martin.soles2211@gmail.com>
2024-12-19 19:53:31 +0100

    yxc

--- analyticGeometry.py
+++ analyticGeometry.py
@@ -1,33 +1,36 @@
 from tkinter import *
 import matplotlib
-
+import math
 
 class Bod:
-    def __init__(self, sX, sY):
-        self.soX = sX
-        self.soY = sY
+    def __init__(self, soX, soY):
+        self.soX = soX
+        self.soY = soY
 
-        self.souradnice = [self.sX, self.sY]
-
-
+        self.souradnice = [self.soX, self.soY]
 
 class Vektor:
     def __init__(self, bodA, bodB):
-        self.slA = bodA[0] - bodB[0]
-        self.slB = bodA[1] - bodB[1]
-
-        self.slozky = [self.slA, self.slB]
-
-
+        self.slozky = [(bodA.souradnice[0])-(bodB.souradnice[0]), (bodA.souradnice[1])-(bodB.souradnice[1])]
 
 class Primka:
     def __init__(self, bod, sVektor):
         self.bod = bod
         self.sVektor = sVektor
 
-        self.nVektorA = [-sVektor[1], sVektor[0]]
-        self.nVektorB = [sVektor[1], -sVektor[0]]
+        self.nVektor = self.sVektor
+        self.nVektor.slozky = self.nVektor.slozky.reverse()
+        #self.nVektor.slozky[0] = (-1) * self.nVektor.slozky[0]
 
-        self.paramR = "X = " + 
+
+bod1 = Bod(4,8)
+bod2 = Bod(9,3)
+
+vektor = Vektor(bod1, bod2)
 
 
+primka = Primka(bod1, vektor)
+print(bod2.souradnice[0])
+print(vektor.slozky)
+#print(primka.bod)
+#print(primka.nVektor.slozky[0])
